81466d1cdbed52b40e0c28eda854fbe8837b1d8c
Diego M. Rodriguez <diego.plan9@gmail.com>
2023-04-08 17:13:02 +0200

    Fix uninlined-format-args

--- crates/lillinput-cli/src/settings.rs
+++ crates/lillinput-cli/src/settings.rs
@@ -129,7 +129,7 @@ pub fn setup_application(opts: Opts, initialize_logging: bool) -> Result<Setting
                     }
                     Err(e) => {
                         log_entries.push(LogEntry::warn(format!(
-                            "Unable to include xdg config file: {:e?}. Skipping it.",
+                            "Unable to include xdg config file: {e:?}. Skipping it.",
                         )));
                     }
                 };
