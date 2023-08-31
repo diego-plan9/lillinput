9e5174e807d8fda83b38a54ec6b9e91557c88061
Diego M. Rodriguez <diego.plan9@gmail.com>
2023-08-31 18:44:01 +0200

    Update to input 0.8 changes

--- crates/lillinput/src/events/libinput.rs
+++ crates/lillinput/src/events/libinput.rs
@@ -2,7 +2,7 @@
 
 use std::fs::{File, OpenOptions};
 use std::os::unix::fs::OpenOptionsExt;
-use std::os::unix::io::{FromRawFd, IntoRawFd, RawFd};
+use std::os::unix::io::OwnedFd;
 use std::path::Path;
 
 use input::LibinputInterface;
@@ -13,19 +13,17 @@ pub struct Interface;
 
 impl LibinputInterface for Interface {
     #[allow(clippy::bad_bit_mask)]
-    fn open_restricted(&mut self, path: &Path, flags: i32) -> Result<RawFd, i32> {
+    fn open_restricted(&mut self, path: &Path, flags: i32) -> Result<OwnedFd, i32> {
         OpenOptions::new()
             .custom_flags(flags)
             .read((flags & O_RDONLY != 0) | (flags & O_RDWR != 0))
             .write((flags & O_WRONLY != 0) | (flags & O_RDWR != 0))
             .open(path)
-            .map(IntoRawFd::into_raw_fd)
+            .map(std::convert::Into::into)
             .map_err(|err| err.raw_os_error().unwrap())
     }
 
-    fn close_restricted(&mut self, fd: RawFd) {
-        unsafe {
-            File::from_raw_fd(fd);
-        }
+    fn close_restricted(&mut self, fd: OwnedFd) {
+        drop(File::from(fd));
     }
 }
